6f01082ba84df6540c0267fe10ca6132b311839f
Chris Smith <chris.smith@pega.com>
2018-03-26 00:38:00 -0400

    Fix ckeditor plugin breaking editor config screen

--- js/plugins/editor_assets/plugin.js
+++ js/plugins/editor_assets/plugin.js
@@ -12,34 +12,39 @@
 
   'use strict';
 
-  CKEDITOR.plugins.add('editor_assets', {
+  var pluginDefinition = {
     icons: null,
     hidpi: false,
+  };
 
-    beforeInit: function(editor) {
-      editor.drupalAssets = Drupal.editor_assets.createManager(editor.name, drupalSettings.editor_assets);
-    },
+  if (Drupal.editor_assets) {
+    _.extend(pluginDefinition, {
+      beforeInit: function(editor) {
+        editor.drupalAssets = Drupal.editor_assets.createManager(editor.name, drupalSettings.editor_assets);
+      },
 
-    init: function(editor) {
-      editor.on('contentDom', function(evt) {
-        if (editor.drupalAssets) {
-          editor.drupalAssets.start(editor.window.$);
-        }
-      });
-      editor.on('contentDomUnload', function(evt) {
-        if (editor.drupalAssets) {
-          editor.drupalAssets.stop();
-        }
-      });
-      editor.on('toDataFormat', function(evt) {
-        var els = evt.data.dataValue.find(function(el) {
-          return el.attributes.class == 'js-editor-assets';
+      init: function(editor) {
+        editor.on('contentDom', function(evt) {
+          if (editor.drupalAssets) {
+            editor.drupalAssets.start(editor.window.$);
+          }
         });
-        _.each(els, function(el) {
-          el.remove();
+        editor.on('contentDomUnload', function(evt) {
+          if (editor.drupalAssets) {
+            editor.drupalAssets.stop();
+          }
         });
-      }, null, null, 8);
-    },
-  });
+        editor.on('toDataFormat', function(evt) {
+          var els = evt.data.dataValue.find(function(el) {
+            return el.attributes.class == 'js-editor-assets';
+          });
+          _.each(els, function(el) {
+            el.remove();
+          });
+        }, null, null, 8);
+      },
+    });
+  }
+  CKEDITOR.plugins.add('editor_assets', pluginDefinition);
 
 })(Drupal, drupalSettings, CKEDITOR);
